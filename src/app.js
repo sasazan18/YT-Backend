8f7df55a8bdcb64d0abe80189fcb71bd0a154bd0
sasazan18 <mcs2409001@xmu.edu.my>
2025-08-26 19:18:13 +0800

    Implement user registration route and controller; update app.js for route integration

--- src/app.js
+++ src/app.js
@@ -6,20 +6,22 @@ const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true 
+    credentials: true
 }))
-
 app.use(express.json({
     limit: "16kb"
 }))
-
 app.use(express.urlencoded({
     extended: true,
     limit: "16kb"
 }))
-
 app.use(express.static("public"))
-
 app.use(cookieParser())
 
-export {app}
+// Routes import 
+import userRoute from "./routes/user.route.js"
+
+// Routes Declaration
+app.use("/api/v1/users", userRoute)
+
+export { app }
