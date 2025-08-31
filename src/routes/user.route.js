2a1848c40c1f843e18d4c92909668f97bf09a9e0
sasazan18 <mcs2409001@xmu.edu.my>
2025-08-31 19:05:34 +0800

    Refactor user routes to use PATCH method for updating user details, avatar, and cover image

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -26,9 +26,9 @@ router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/me").get(verifyJWT, getCurrentUser);
-router.route("/update-user-details").post(verifyJWT, updateUserDetails);
-router.route("/update-avatar").post(verifyJWT, upload.single("avatar"), updateUserAvatar);
-router.route("/update-cover-image").post(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/update-user-details").patch(verifyJWT, updateUserDetails);
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/update-cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/upload-video").post(verifyJWT, upload.fields([
     {
